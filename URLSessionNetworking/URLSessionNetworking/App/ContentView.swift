1a36391ff43430c5fe484a210f37fe444511e57a
이준영 <junlight94@gmail.com>
2026-01-05 22:04:08 +0900

    init

---
+++ URLSessionNetworking/URLSessionNetworking/App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URLSessionNetworking
+//
+//  Created by Junyoung on 1/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLSessionNetworking/URLSessionNetworking/App/URLSessionNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionNetworkingApp.swift
+//  URLSessionNetworking
+//
+//  Created by Junyoung on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
