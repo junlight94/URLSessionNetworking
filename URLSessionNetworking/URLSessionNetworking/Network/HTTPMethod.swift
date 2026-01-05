ae042c7ffb6111b926242d1a581543d3219910de
이준영 <junlight94@gmail.com>
2026-01-05 22:11:07 +0900

    feat. swift6 변경 HTTPMethod 추가

---
+++ URLSessionNetworking/URLSessionNetworking/Network/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  URLSessionNetworking
+//
+//  Created by Junyoung on 1/5/26.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
