e651008a2c204a174ff5f483bee200856c93523f
이준영 <junlight94@gmail.com>
2026-01-14 22:35:39 +0900

    feat. request 코드 session으로 이동

--- URLSessionNetworking/URLSessionNetworking/Network/Client/Session.swift
+++ URLSessionNetworking/URLSessionNetworking/Network/Client/Session.swift
@@ -20,6 +20,50 @@ public struct Session: Sendable {
         self.configuration = configuration
         self.interceptor = interceptor
     }
+    
+    /// 요청을 어댑터로 수정 후 전송하고, 실패 시 재시도 로직 수행
+    /// - Parameters:
+    ///   - originalRequest: 원본 요청
+    ///   - retryCount: 현재 재시도 횟수
+    /// - Returns: 응답 데이터와 URLResponse
+    func requestData(
+        _ originalRequest: URLRequest,
+        retryCount: Int = 0
+    ) async throws -> (data: Data, urlResponse: URLResponse)  {
+        let adaptedRequest = try await interceptor.adapt(originalRequest)
+
+        do {
+            let (data, urlResponse) = try await session.data(for: adaptedRequest)
+            return (data, urlResponse)
+        } catch {
+            let decision = try await interceptor.retry(
+                adaptedRequest,
+                dueTo: error,
+                retryCount: retryCount
+            )
+            
+            switch decision {
+            case .retry:
+                return try await requestData(
+                    originalRequest,
+                    retryCount: retryCount + 1
+                )
+                
+            case .retryWithDelay(let delay):
+                try await Task.sleep(for: .seconds(delay))
+                return try await requestData(
+                    originalRequest,
+                    retryCount: retryCount + 1
+                )
+                
+            case .doNotRetry:
+                throw error
+                
+            case .doNotRetryWithError(let error):
+                throw error
+            }
+        }
+    }
 }
 
 extension Session {
